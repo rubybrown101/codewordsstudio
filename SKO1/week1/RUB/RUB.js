c27e05588d49553d35182eb21292aae399b0a54c
rubybrown101 <68985063+rubybrown101@users.noreply.github.com>
2020-10-20 14:39:36 +1100

    Add files via upload

---
+++ SKO1/week1/RUB/RUB.js
@@ -0,0 +1,27 @@
+function setup() {
+createCanvas (800,500);
+background (255,90,70);
+strokeWeight (1);
+fill (0);
+ellipse (150,150,200,200);
+strokeWeight (50);
+rect (50,50,50,400);
+line (100, 250, 300,450);
+strokeWeight (0);
+fill (255,90,70);
+rect (125,50,20,150);
+rect (125,125,20,300)
+strokeWeight (0);
+fill (255,90,70);
+arc (160,150,70,70,180, 40, PI, PI + OPEN );
+strokeWeight (30);
+arc (260, 250, 70, 120, 270.1, 160.2, PI, QUARTER_PI, OPEN);
+strokeWeight (0);
+rect (210, 230, 120, 20);
+rect (0, 440, 440, 40);
+}
+
+
+function draw() {
+
+}
