a81a6307a2d210c14ea9eaa23aa4698b81b0fba0
rubybrown101 <68985063+rubybrown101@users.noreply.github.com>
2020-10-20 14:40:26 +1100

    Add files via upload

---
+++ SKO1/week1/B/B.js
@@ -0,0 +1,14 @@
+function setup() {
+createCanvas (400,600);
+background(200,30,167);
+}
+
+
+function draw() {
+  fill(0);
+  ellipse(150,120,220,220);
+  ellipse(180,370,300,300);
+ 
+rect (20,20,100,500);
+
+}
