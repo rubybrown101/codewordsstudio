b558a9d8614081992a80205126b58849a42598f9
rubybrown101 <68985063+rubybrown101@users.noreply.github.com>
2020-10-20 14:15:31 +1100

    Add files via upload

---
+++ SKO1/week1/R/R.js
@@ -0,0 +1,15 @@
+function setup() {
+createCanvas (500,500);
+
+background (65,28,84);
+stroke (255);
+strokeWeight (5);
+line (100,100,100,400);
+line (100,250,300,400);
+}
+
+
+function draw() {
+  circle (200,200,200,400);
+
+}
